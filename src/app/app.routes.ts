da01016f6734d164a44278b48f129938d179f414
taoista <taoista-@hotmail.com>
2025-01-26 13:11:23 -0300

    trabanbajdo en el hombre

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,17 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from '@pages/home/home.component';
+import { MainLayoutComponent } from '@shared/components/main-layout/main-layout.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        component: MainLayoutComponent,
+        children: [
+            { 
+                path: '', 
+                component: HomeComponent 
+            },
+          
+        ]
+    }
+];
