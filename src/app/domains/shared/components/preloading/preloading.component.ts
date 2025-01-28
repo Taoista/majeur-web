73180acaf9a42e2e4bffb414a09ca1e97131a2a5
taoista <taoista-@hotmail.com>
2025-01-28 04:07:55 -0300

    correccion d software

---
+++ src/app/domains/shared/components/preloading/preloading.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-preloading',
+  standalone: true,
+  imports: [],
+  templateUrl: './preloading.component.html',
+  styleUrl: './preloading.component.css'
+})
+export class PreloadingComponent {
+
+}
